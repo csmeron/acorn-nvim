6a54a65cf455775140b1fcdf67f7b7735c7b6e0c
csmeron <csmeron@proton.me>
2026-08-22 20:24:17 -0500

    Basic Lazy set up and theme installation (Oxocarbon.nvim)

--- init.lua
+++ init.lua
@@ -3,12 +3,23 @@
 -- Uses lazy.nvim for plugin management
 --]]
 
+
 -- Import core modules
 local core = {
   "core.options",
   "core.keymaps",
 }
 
+local plugs = {
+  { import = "plugins.ui" } ,
+}
+
+-- Send plugins to the Lazy bootstrap
+require("core.lazy").setup(plugs)
+-- Requires all core modules
 for _, mod in ipairs(core) do
   require(mod)
 end
+
+-- colorscheme (temporary until mangement plugin is added)
+vim.cmd.colorscheme "oxocarbon"
---
+++ lua/plugins/ui/themes.lua
@@ -0,0 +1,14 @@
+--[[
+-- Spec for setting up colorschemes
+--]]
+
+local theme_list = {
+  "oxocarbon",
+}
+
+return {
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+    build = false,
+  }
+}
